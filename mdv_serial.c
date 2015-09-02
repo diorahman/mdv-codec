c0ccbbc20fa65f1684c292e3e9f948e0e94d9837
Dhi Aurrahman <diorahman@rockybars.com>
2015-09-02 08:05:07 +0700

    Add debug handler when the read_cb is NULL

--- mdv_serial.c
+++ mdv_serial.c
@@ -12,6 +12,7 @@
 #define MDV_BAUD_RATE B4800
 
 static int mdv_serial_wait_for_header(int fd, unsigned char *buf);
+static void mdv_serial_read_debug(int fd, unsigned char *buf);
 
 static struct termios tio_original_opts;
 
@@ -56,16 +57,19 @@ void mdv_serial_write(int fd, unsigned char *buf) {
 void mdv_serial_read(int fd, unsigned char *buf, void (*read_cb)(int, unsigned char *)) {
     int rc = -1;
     int hc = -1;
-    int i; 
     do {
         while(hc < 0) {
             hc = mdv_serial_wait_for_header(fd, buf);  
         }
-        rc = read(fd, buf, MDV_DATA_LEN);
-        (*read_cb)(fd, buf);
+        rc = read(fd, buf, MDV_DATA_LEN);       
+        if (read_cb != NULL) {
+            return (*read_cb)(fd, buf);
+        }
+        mdv_serial_read_debug(fd, buf);
     }
     while(rc == MDV_DATA_LEN && (buf[0] == 0x4C && buf[1] == 0x4E));
-    printf("data dropped, with header: %.2x %2.x\n", buf[0], buf[1]);
+    
+    fprintf(stderr, "%s: data dropped, with header: %.2x %2.x\n", __FILE__, buf[0], buf[1]);
     mdv_serial_read(fd, buf, read_cb);
 }
 
@@ -93,3 +97,10 @@ int mdv_serial_wait_for_header(int fd, unsigned char *buf) {
     return -1;
 }
 
+void mdv_serial_read_debug(int fd, unsigned char *buf) {
+    int i;
+    for (i = 0; i < MDV_DATA_LEN; i++) {
+        printf("%.2x", buf[i]);
+    }
+    printf("\n");
+}
